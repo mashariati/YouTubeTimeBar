68354356974618cfcb7b0b0eaba238c58b52aeb1
mohammadamin <aminshariati1383@gmail.com>
2024-10-08 15:45:38 +0330

    update exoplayer to media3.exoplayer.ExoPlayer 1.4.1

--- library/src/main/java/com/github/vkay94/timebar/YouTubeTimeBar.kt
+++ library/src/main/java/com/github/vkay94/timebar/YouTubeTimeBar.kt
@@ -14,10 +14,10 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
-import com.google.android.exoplayer2.C
-import com.google.android.exoplayer2.ui.TimeBar
-import com.google.android.exoplayer2.util.Assertions
-import com.google.android.exoplayer2.util.Util
+import androidx.media3.common.C
+import androidx.media3.ui.TimeBar
+import androidx.media3.common.util.Assertions
+import androidx.media3.common.util.Util
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.math.max
